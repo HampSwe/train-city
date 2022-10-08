2171d758a5007216c2a1729cc34716dbe4e9eb30
Hampus Serneke <hampus.serneke@gmail.com>
2022-10-08 20:36:40 +0200

    Add fillTriangle

--- src/Graphics.scala
+++ src/Graphics.scala
@@ -36,4 +36,26 @@ class Graphics(
 
     /** Initializes the world */
     def drawWorld(): Unit =
-        pixelWindow.fill(10, 10, 100, 100, Colors.green)
+        //pixelWindow.fill(10, 10, 100, 100, Colors.green)
+
+        val p1 = Pos(10, 10)
+        val p2 = Pos(50, 70)
+
+        drawWideLine(p1, p2, 30, Colors.black)
+
+        pixelWindow.line(p1.x, p1.y, p2.x, p2.y, Colors.red)
+    
+
+    def drawWideLine(p1: Pos, p2: Pos, width: Int, color: JColor): Unit =
+        pixelWindow.line(p1.x, p1.y, p2.x, p2.y, color, width)
+
+    def fillTriangle(p1: Pos, p2: Pos, p3: Pos, color: JColor): Unit =
+        val deltaX = p3.x - p2.x
+        val deltaY = p3.y - p2.y
+        val distance = math.hypot(deltaX, deltaY).ceil.toInt
+
+        def x(t: Int): Int = (p3.x + deltaX * (t / distance)).round.toInt
+        def y(t: Int): Int = (p3.y + deltaY * (t / distance)).round.toInt
+        
+        for t <- 0 to distance do
+            pixelWindow.line(p1.x, p1.y, x(t), y(t))
