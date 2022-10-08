7ac12260c9160869e5cb765e57c14fcd91450b76
Hampus Serneke <hampus.serneke@gmail.com>
2022-10-08 18:57:47 +0200

    Add inputHandler

--- src/Game.scala
+++ src/Game.scala
@@ -4,6 +4,8 @@
 
 package traincity
 
+import java.beans.EventHandler
+
 /* Declares and initializes some constant variables containing info about the game */
 object Game:
     val title: String = "Train City" /* The title of the game */
@@ -21,8 +23,13 @@ class Game:
         // Creates an instance of the CityWindow class
         val cityWindow = new CityWindow(title, width, height) 
 
-        // Creates an instance, graphics, of the Graphics class. The instance is used to perform all screen operations on the cityWindow.
+        // Creates an instance, graphics, of the Graphics class.
+        // This instance is used to perform all the screen operations on the cityWindow.
         val graphics = new Graphics(cityWindow)
+
+        // Creates an instance, inputHandler, of the InputHandler class.
+        //This instance handles all the input events that are triggered by the cityWindow
+        val inputHandler = new InputHandler(cityWindow)
 
         graphics.drawWorld()
 
