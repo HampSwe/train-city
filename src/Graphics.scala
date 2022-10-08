8c1102ad5ec4427cc6cbd530edcf2a92f0ac2729
Hampus Serneke <hampus.serneke@gmail.com>
2022-10-08 17:16:30 +0200

    Add Graphics.scala

---
+++ src/Graphics.scala
@@ -0,0 +1,13 @@
+package traincity
+
+class Graphics(
+    val width: Int, /*The width of the window in px*/
+    val height: Int, /*The height of the window in px*/
+    val aspectRatio: Double = 0.715, /* aspectRatio represents the value of (height / width) */
+    ):
+    
+    import Graphics.*
+
+
+object Graphics:
+    ???
