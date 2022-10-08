9891b42357f2f56d742b98f12817bf1f9ca7114a
Hampus Serneke <hampus.serneke@gmail.com>
2022-10-08 17:37:57 +0200

    Add Game.scala and improve Graphics.scala

---
+++ src/Game.scala
@@ -0,0 +1,14 @@
+/**
+  * This file handles the game loop.
+  */
+
+package traincity
+
+object Game:
+    val title: String = "Train City"
+    val width: Int = 1000 /*The width of the window in px*/
+    val aspectRatio: Double = 0.7153 /* aspectRatio represents the value of (height / width) */
+    val height: Int = (width * aspectRatio).round.toInt
+
+class Game:
+    ???
--- src/Graphics.scala
+++ src/Graphics.scala
@@ -1,13 +1,33 @@
+/**
+  * This file handles the game graphics.
+  * 
+  * Read more about the package "PixelWindow" here:
+  * https://fileadmin.cs.lth.se/pgk/api/api/introprog/PixelWindow.html
+  * 
+  */
+
 package traincity
-
-class Graphics(
-    val width: Int, /*The width of the window in px*/
-    val height: Int, /*The height of the window in px*/
-    val aspectRatio: Double = 0.715, /* aspectRatio represents the value of (height / width) */
-    ):
-    
-    import Graphics.*
-
+import java.awt.Color as JColor
 
 object Graphics:
-    ???
+
+    /* This objects stores the RGB-values of the java.awt.Colors that are used */
+    object Colors:
+        val white = new JColor(255, 255, 255)
+        val black = new JColor(0, 0, 0)
+        val red = new JColor(255, 0, 0)
+        val green = new JColor(0, 255, 0)
+        val blue = new JColor(0, 0, 255)
+        val background = new JColor(255, 255, 255)
+        val foreground = new JColor(0, 0, 0)
+
+class Graphics(
+    val windowTitle: String, /* The title of the window */
+    val width: Int, /* The width of the window in px */
+    val height: Int, /* The height of the window in px */
+    ):
+
+    import Graphics.*
+    import introprog.PixelWindow
+
+    val pixelWindow = new PixelWindow(width, height, windowTitle, Colors.background, Colors.foreground)
