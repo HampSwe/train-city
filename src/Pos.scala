7fb4f5d48e585c78f5f75f4bc7dbb9e2f3ef80c6
Hampus Serneke <hampus.serneke@gmail.com>
2022-10-08 19:31:10 +0200

    Add Pos.scala

---
+++ src/Pos.scala
@@ -0,0 +1,12 @@
+/**
+  * This file handles positions in the game.
+  */
+
+package traincity
+
+/** This class represents a position in the game as two coordinates.
+    The coordinates are currently in a 1:1 relationship with the window's pixels*/
+class Pos(
+    val x: Int,
+    val y: Int,
+)
