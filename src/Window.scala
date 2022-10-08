45ed052d363232a29d922812992e649d3f7bef00
Hampus Serneke <hampus.serneke@gmail.com>
2022-10-08 18:19:22 +0200

    Add Window.scala

---
+++ src/Window.scala
@@ -0,0 +1,23 @@
+/**
+  * This file handles the game window
+  * 
+  * Read more about the package "PixelWindow" here:
+  * https://fileadmin.cs.lth.se/pgk/api/api/introprog/PixelWindow.html
+  */
+
+package traincity
+import java.awt.Color as JColor
+
+object Window:
+    ???
+
+class Window(
+    val windowTitle: String, /* The title of the window */
+    val width: Int, /* The width of the window in px */
+    val height: Int, /* The height of the window in px */
+    ):
+
+    import Window.*
+    import introprog.PixelWindow
+
+    val pixelWindow = new PixelWindow(width, height, windowTitle, Graphics.Colors.background, Graphics.Colors.foreground)
